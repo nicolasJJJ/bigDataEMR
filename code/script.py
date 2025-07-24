2ad1ebef068e27248da0dbc87453524c8ce9cfa4
warcrime <warcrime@LAPTOP-QSH8Q4VD>
2025-07-24 13:50:55 +0200

    init

---
+++ code/script.py
@@ -0,0 +1,51 @@
+from pyspark.sql import SparkSession
+import os
+import opendatasets as od
+from pyspark.sql.functions import col, length, instr, when
+import boto3
+
+session = boto3.Session(region_name='eu-west-3')
+ssm = session.client('ssm')
+
+usr = ssm.get_parameter(Name="/kaggle/username", WithDecryption=True)
+key = ssm.get_parameter(Name="/kaggle/key", WithDecryption=True)
+
+with open("kaggle.json", "w") as text_file:
+    print(f'{"username":"{usr}","key":"{key}"}', file=text_file)
+
+od.download(
+    "https://www.kaggle.com/datasets/dschettler8845/the-pile-dataset-part-00-of-29",
+    "../data"
+)
+
+
+file_path = os.path.abspath("../data/the-pile-dataset-part-00-of-29/00.jsonl")
+
+if not os.path.exists(file_path):
+    raise FileNotFoundError(f"fichier introuvable à {file_path}")
+
+
+spark = SparkSession.builder \
+    .appName("EMR_spark") \
+    .getOrCreate()
+
+spark.sparkContext.setLogLevel("WARN")
+
+
+df = spark.read.json(file_path)
+df.printSchema()
+df.show()
+
+
+df = df.filter(length(col("text")) > 100)\
+    .filter(~instr(col("text"),'copyright') > 0)\
+    .withColumn('set_name', col("meta.pile_set_name"))\
+    .drop('meta')
+
+df.write \
+  .partitionBy("set_name") \
+  .mode("overwrite") \
+  .option("compression", "snappy") \
+  .parquet("s3a://sparkresultsjjj/thepile_cleaned/")
+
+spark.stop()
