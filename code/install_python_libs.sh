48eaf45874fa4df3e4761bd81dcd9e4d40a33fff
warcrime <warcrime@LAPTOP-QSH8Q4VD>
2025-08-03 23:45:20 +0200

    change of bootstrap mkdir position

--- code/install_python_libs.sh
+++ code/install_python_libs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo pip3 install --upgrade boto3 opendatasets
-sudo -u hadoop mkdir -p /home/hadoop/.kaggle
-sudo -u hadoop chmod 700 /home/hadoop/.kaggle
+sudo -u hadoop mkdir -p /home/.kaggle
+sudo -u hadoop chmod 700 /home/.kaggle
